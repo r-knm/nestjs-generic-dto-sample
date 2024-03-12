97b96864d31613ca705fea47c3ab8adcaf9e2961
r-knm <kainuma@log-build.com>
2024-03-12 18:00:33 +0900

    500エラーを強制出力

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    throw new InternalServerErrorException('Internal server error!!');
+
     return 'Hello World!';
   }
 }
